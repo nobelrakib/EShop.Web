2964660c678330c798cb9e1e9d1827af55027464
Zahid Ahmed <zahidbiscj@gmail.com>
2021-04-05 22:51:28 +0600

    all code removed . starting from stratch

--- src/app/shared/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-
-export const content: Routes = [
-  {
-    path: 'sample-page',
-    loadChildren: () => import('../../components/sample/sample.module').then(m => m.SampleModule)
-  }
-];
