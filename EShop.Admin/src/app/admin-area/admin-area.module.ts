7fe7f8e4c417927f63c7c4a11682ea19a73e4c56
Zahid Ahmed <zahidbiscj@gmail.com>
2021-06-06 23:48:51 +0600

    ngx-bootstrap datepicker installed successfully with all error fixed

--- EShop.Admin/src/app/admin-area/admin-area.module.ts
+++ EShop.Admin/src/app/admin-area/admin-area.module.ts
@@ -1,9 +1,11 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {AdminRoutingModule } from './admin-area-routing.module';
 import { EShopAdminComponent } from './components/eshop-admin/eshop-admin.component';
 import {SharedAdminModule} from './shared/shared-admin.module';
 import { RoleComponent } from './role/role.component';
 import { RoleAddComponent } from './role-add/role-add.component';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @NgModule({
     declarations: [
@@ -14,7 +16,7 @@ import { RoleAddComponent } from './role-add/role-add.component';
     imports: [
         AdminRoutingModule,
         SharedAdminModule,
-        
+        BsDatepickerModule.forRoot(),
     ],
   })
   export class AdminModule { }
